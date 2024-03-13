a5b8f7f974b3dfff359f06430b3ea27cdd80132e
Josh-Twomey <140522624+Josh-Twomey@users.noreply.github.com>
2024-03-13 11:05:06 +0200

    Update DWA - 11

--- DWA-11/scripts.js
+++ DWA-11/scripts.js
@@ -3,11 +3,12 @@ import { getState, dispatch } from "./models/store.js";
 
 console.log(getState())
 
-
 dispatch(increase());
 dispatch(increase());
 console.log(getState());
+
 dispatch(decrease());
 console.log(getState());
+
 dispatch(reset());
 console.log(getState());
